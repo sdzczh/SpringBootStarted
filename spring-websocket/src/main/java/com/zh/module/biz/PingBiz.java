d322cec516d734f5a5111dac483e45ca1d0a8867
昭和 <zgsdzczh@163.com>
2019-10-21 20:33:02 +0800

    ws

--- spring-websocket/src/main/java/com/zh/module/biz/PingBiz.java
+++
@@ -1,7 +0,0 @@
-package com.zh.module.biz;
-
-import io.netty.channel.Channel;
-
-public interface PingBiz {
-    void ping(Channel channel, String cmsgCode);
-}
---
+++ websocket/src/main/java/com/zh/module/controller/StartWebSocket.java
@@ -0,0 +1,43 @@
+package com.zh.module.controller;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhaohe
+ */
+@Component
+@Slf4j
+public class StartWebSocket implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        action();
+    }
+    private static void action(){
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            //开启服务端
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(eventLoopGroup,workGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childHandler(new MyWebSocketChannelHandler());
+            log.info("服务端开启等待客户端连接..");
+            Channel channel = serverBootstrap.bind(1606).sync().channel();
+            channel.closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            //退出程序
+            eventLoopGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
